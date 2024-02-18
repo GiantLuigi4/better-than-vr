e7bc40b1895a0960f1024d2b7ef3aac54cee83e4
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2024-02-18 00:26:26 -0500

    shoulda removed this

--- src/main/java/tfc/btvr/util/config/gui/menu/MenuCaptureComponent.java
+++ src/main/java/tfc/btvr/util/config/gui/menu/MenuCaptureComponent.java
@@ -1,6 +1,5 @@
 package tfc.btvr.util.config.gui.menu;
 
-import net.minecraft.client.gui.GuiTexturedButton;
 import net.minecraft.client.gui.options.components.ButtonComponent;
 import net.minecraft.core.lang.I18n;
 import tfc.btvr.util.config.gui.helper.MenuHelper;
@@ -26,17 +25,6 @@ public class MenuCaptureComponent extends ButtonComponent {
 	
 	@Override
 	protected void renderButton(int x, int y, int relativeButtonX, int relativeButtonY, int buttonWidth, int buttonHeight, int relativeMouseX, int relativeMouseY) {
-		GuiTexturedButton applyL = new GuiTexturedButton(
-				0, "/gui/gui.png", buttonWidth, 0,
-				0, 86 - 20 * 2,
-				buttonWidth / 2, 20
-		);
-		GuiTexturedButton applyR = new GuiTexturedButton(
-				0, "/gui/gui.png", buttonWidth, 0,
-				200 - (buttonWidth / 2), 86 - 20 * 2,
-				buttonWidth / 2, 20
-		);
-		
 		relativeMouseX -= relativeButtonX;
 		
 		boolean hover = false;
